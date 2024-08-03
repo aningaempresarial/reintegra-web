b409b81900f31f5ca4e00876e1dac51c5c2db5aa
Sabrina Cristan <sabrinacristan@gmail.com>
2024-08-02 21:57:47 -0300

    🖌️ Criando telas

---
+++ resources/views/home.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.layout')
+@section('titulo', 'Reintegra | Home')
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/home.css') }}">
+@endsection
+@section('style')
+<!-- Não deu para colocar a imagem de fundo pelo css, por isso está aqui -->
+<style>
+    .home-doacao {
+        background-image: url("{{ asset('images/fundo.svg') }}");
+    }
+</style>
+@endsection
+@section('conteudo')
+    <!-- Seção de doação -->
+    <section id="doacao" class="home-doacao">
+        <div>
+            <img src="{{ asset('images/img-doacao.png') }}">
+        </div>
+        <div>
+            <h1>Pix Reintegra</h1>
+            <p>Fortaleça organizações que quebram o ciclo da criminalidade.</p>
+            <a href="">Doar</a>
+        </div>
+    </section>
+    <!-- Seção de divulgação do app -->
+    <section id="app">
+        <h1 class='title-app'>Conheça o app Reintegra!</h1>
+        <div class="home-app">
+            <div>
+                <img src="{{ asset('images/img-app.png') }}" width="400">
+            </div>
+            <div>
+                <h1>Disponível para baixar em:</h1>
+                <a href=""><img src="{{ asset('images/img-googleplay.png') }}" width="200"></a>
+                <a href=""><img src="{{ asset('images/img-appstore.png') }}" width="200"></a>
+            </div>
+        </div>
+    </section>
+@endsection
+
---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,21 @@
+<nav class="navbar sticky-top navbar-expand-lg">
+    <div class="container-fluid">
+        <img class="logo" src="{{ asset('images/logo.png') }}" alt="Navbar">
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Sobre nós</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Cadastro</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Login</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
