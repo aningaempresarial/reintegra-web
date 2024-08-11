5fe2a335045137747be49e96318908ad4c015f56
Sabrina Cristan <sabrina.cristan@gmail.com>
2024-08-11 11:29:26 -0300

    🪄 Criando dashboard

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.dashboard-layout')
+@section('titulo', 'Reintegra | Dashboard')
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/dashboard.css') }}">
+@endsection
+@section('conteudo')
+
+@endsection
---
+++ resources/views/partials/dashboard-navbar.blade.php
@@ -0,0 +1,7 @@
+<div class="border-right bg-light" id="sidebar-wrapper">
+    <div class="list-group list-group-flush">
+        <a href="#" class="list-group-item list-group-item-action bg-light"><img src="{{ asset('images/logo-icon.png') }}" width="60px"></a>
+        <a href="#" class="list-group-item list-group-item-action bg-light"><img src="{{ asset('images/home-icon.png') }}" width="60px"></a>
+        <a href="#" class="list-group-item list-group-item-action bg-light"><img src="{{ asset('images/read-icon.png') }}" width="60px"></a>
+    </div>
+</div>
