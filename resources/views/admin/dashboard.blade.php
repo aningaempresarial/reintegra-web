b9581dab6bdb565c9db5608cf210a677e580b290
Sabrina Cristan <sabrina.cristan@gmail.com>
2024-08-11 22:18:14 -0300

    📄 Estruturando dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -4,5 +4,8 @@
 <link rel="stylesheet" href="{{ asset('css/dashboard.css') }}">
 @endsection
 @section('conteudo')
-
+<section>
+    <h1>Bem vindo, Administrador!</h1>
+    <div class="panel"></div>
+</section>
 @endsection
