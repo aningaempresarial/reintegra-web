28d32c829ec06ff49e0f5d572b77185564e2e886
Sabrina Cristan <sabrina.cristan@gmail.com>
2024-08-16 22:28:28 -0300

    🖋️ Renomeando e organizando arquivos

---
+++ resources/views/partials/navbar-admin.blade.php
@@ -0,0 +1,9 @@
+<div class="border-right bg-light sidebar" id="sidebar-wrapper">
+    <div class="list-group list-group-flush">
+        <a href="{{ url('/admin/dashboard') }}" class="list-group-item list-group-item-action bg-light"><img src="{{ asset('images/logo-icon.png') }}" width="60px"></a>
+        <a href="{{ url('/admin/dashboard') }}" class="list-group-item list-group-item-action bg-light"><img src="{{ asset('images/home-icon.png') }}" width="60px"><p>Início</p></a>
+        <a href="{{ url('/admin/users') }}" class="list-group-item list-group-item-action bg-light"><img src="{{ asset('images/user-icon.png') }}" width="60px"><p>Gerenciar usuários</p></a>
+        <a href="{{ url(' ') }}" class="list-group-item list-group-item-action bg-light"><img src="{{ asset('images/read-icon.png') }}" width="60px"><p>Painel de relatórios</p></a>
+        <a href="{{ url('/admin/config') }}" class="list-group-item list-group-item-action bg-light"><img src="{{ asset('images/config-icon.png') }}" width="60px"><p>Configurações</p></a>
+    </div>
+</div>
