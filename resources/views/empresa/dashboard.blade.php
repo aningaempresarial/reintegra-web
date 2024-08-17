53aa5ceab6aef7796beb634e1768d34161ac049c
Sabrina Cristan <sabrina.cristan@gmail.com>
2024-08-16 22:31:52 -0300

    🏗️ Área da empresa

---
+++ resources/views/empresa/dashboard.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.dashboard-layout')
+@section('titulo', 'Reintegra | Dashboard')
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/dashboard.css') }}">
+@endsection
+@section('conteudo')
+<div class="container-dashboard">
+    <h1 class="page-title">É um prazer recebê-los, Empresa X!</h1>
+    <div class="panel cards-panel">
+        <div class="card c1">
+            <div class="card-body">
+                This is some text within a card body.
+            </div>
+        </div>
+        <div class="card c2">
+            <div class="card-body">
+                This is some text within a card body.
+            </div>
+        </div>
+        <div class="card c3">
+            <div class="card-body">
+                This is some text within a card body.
+            </div>
+        </div>
+        <div class="card c4">
+            <div class="card-body">
+                This is some text within a card body.
+            </div>
+        </div>
+    </div>
+    <div class="panel"></div>
+    @include('partials.footer-dashboard')
+</div>
+@endsection
