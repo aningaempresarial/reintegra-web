b1a98d79bc4f1fa08852684fa0c034461f9a2af3
Sabrina Cristan <sabrina.cristan@gmail.com>
2024-08-17 15:27:04 -0300

    ✨ Adicionando barra de perfil

---
+++ resources/views/partials/profilebar.blade.php
@@ -0,0 +1,6 @@
+<div class="profilebar">
+    <div class="profile-info">
+        <p>Martha</p>
+        <img class="profile-photo" src="{{ asset('images/profile-photo.png') }}">
+    </div>
+</div>
