a40e3939e39928278f05d0efe2fca3c29ecdf887
Sabrina Cristan <sabrina.cristan@gmail.com>
2024-10-08 22:59:56 -0300

    Centralizando modais

--- resources/views/empresa/edit.blade.php
+++ resources/views/empresa/edit.blade.php
@@ -1,5 +1,5 @@
 <div class="modal fade" id="editModalEmpresa" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
+    <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header">
                 <h1 class="modal-title fs-5" id="exampleModalLabel">Editar meus dados</h1>
--- resources/views/user/editpass.blade.php
+++ resources/views/user/editpass.blade.php
@@ -1,5 +1,5 @@
 <div class="modal fade" id="editModalPass" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
+    <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header">
                 <h1 class="modal-title fs-5" id="exampleModalLabel">Atualizar senha</h1>
